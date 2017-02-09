31f11722fff9c4f83bdc426c9924527e7cf60c88
Yaroslav Cherednyk <azazely85@gmail.com>
2017-02-09 18:04:04 +0200

    mysqli_real_escap_string

--- test.php
+++ test.php
@@ -5,7 +5,7 @@ $link=mysqli_connect('localhost','u_yardev','NDj13xpH','yardev');
 //pre($link);
 
 //$res=mysqli_query($link, 'SET NAMES "utf-8"');
-
+$name = mysqli_real_escape_string($link,"K'dsdf");
 $sql ="SELECT * FROM teachers";
 $result = mysqli_query($link,$sql);
 while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)){
